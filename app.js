eeb4267077e5501357b0ba814cf54c91592f6b9a
kimyohun <yohunkim0@gmail.com>
2025-03-04 17:42:26 +0900

    테스트 후 수정

---
+++ app.js
@@ -0,0 +1,64 @@
+const http = require('http');
+const fs = require('fs');
+const qs = require('querystring');
+// const { Buffer } = require('node:buffer');
+
+
+const server = http.createServer(function(request, response){
+    console.log("테스트 요청 방식 :",request.method);
+    console.log("테스트 요청 URL :",request.url);
+    // 테스트 요청 방식 : GET
+    // 테스트 요청 URL : /
+
+    if(request.method === 'GET'){
+        if(request.url === '/'){
+            // 최초접속시도 확인
+            console.log("최초접속시도");
+
+            response.statusCode = 200; // OK
+            response.setHeader('Content-Type', 'text/html; charset=utf-8');
+            const data = fs.readFileSync("./index.html");
+            response.write(data);
+            response.end();
+        }
+    }
+    if(request.method === 'POST'){
+        if(request.url === '/yohoon'){
+            // * request가 data가 들어오면 실행
+            request.on('data',function(data){
+
+                // console.log(data.toString('utf8',0,20));
+                // const buf1 = Buffer.from(data.toString(),'utf8');
+                // console.log(buf1.toString());
+
+                // * name과 value를 분리 -> 문자열 변환 후 '=' 기준으로 분리
+                let dataTrans = data.toString().split('=');
+                console.log(dataTrans[1]);
+
+                // console.log(data.toString());   
+
+                // const dataGet = data.toString('utf8');
+
+
+
+                // let textHTML = `<!DOCTYPE html>
+                // <html lang="en">
+                // <head>
+                //     <meta charset="UTF-8">
+                //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                //     <title>Document</title>
+                // </head>
+                // <body>
+                //     <h1>${data.toString()}</h1>
+                // </body>
+                // </html>`;
+
+                // fs.writeFileSync(`test.html`,textHTML);
+            })
+        }
+    }
+})
+
+server.listen(3030,function(){
+    console.log("http://localhost:3030");
+})
